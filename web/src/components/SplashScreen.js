93121d6787a91af2374188e0f71b702be78a91c7
jeffrey <jeffrey.kingsbury@icloud.com>
2022-09-29 17:31:00 -0400

    Task B complete
    Added the following packages:
    React-Markdown - (To allow adding markdown safely)
    remark-gfm - (markdown styling)
    
    TODO:
    Styling. The application remains without style due to time constraints.
    
    REMOVED:
    .prettierignore
    
    Application has been linted and formatted using yarn lint and yarn prettier-write

--- web/src/components/SplashScreen.js
+++ web/src/components/SplashScreen.js
@@ -5,10 +5,14 @@ import ViewRepo from './ViewRepo';
 const SplashScreen = () => {
   //Store the data in a state to allow for conditional renders.
   const [repoData, setRepoData] = useState(false);
-  const [selectedRepo, setSelectedRepo] = useState();
+  const [renderedRepos, setRenderedRepos] = useState(false);
+  const [selectedRepo, setSelectedRepo] = useState(false);
+  const [usedLanguages, setUsedLanguages] = useState(false);
+  const [filter, setFilter] = useState(false);
 
   // Storing the data retreieved from /repos as a state, allowing conditional rendering.
   useEffect(() => {
+    const languages = new Set();
     //FETCH the data from /repos, sort it in reverse-chrono, and store it in a state.
     const getInfoFromApi = async () => {
       await fetch('/repos')
@@ -27,23 +31,65 @@ const SplashScreen = () => {
           }
 
           const sortedInReverseChronological = res.sort((a, b) => {
+            languages.add(a.language);
             return new Date(b.created_at) - new Date(a.created_at);
           });
+
+          //Convert the Set of languages found to an array for later.
+          setUsedLanguages(Array.from(languages));
+          //store the filtered array
           setRepoData(sortedInReverseChronological);
+          //On load were storing the filtered array here as well, and using this to render.
+          //Later we can use filter on the repoData to change what is rendered.
+          setRenderedRepos(sortedInReverseChronological);
         });
     };
     getInfoFromApi();
   }, []);
 
+  //Function to filter the rendered repos by language
+  const filterBy = (language) => {
+    //If the filter is already active and the user clicks the button again, remove the filter.
+    if (filter === language) {
+      setFilter(false);
+      setRenderedRepos(repoData);
+      return;
+    }
+
+    //Filter the languages.
+    setFilter(language);
+    const filtered = repoData.filter((e) => {
+      return e.language === language;
+    });
+
+    setRenderedRepos(filtered);
+  };
+
   return (
     <Wrapper>
       {!repoData && 'Loading. Please wait.'}
       {repoData === 400 && 'An error occurred. Please try again.'}
-      {repoData && !selectedRepo && repoData !== 400 && (
+      {renderedRepos && !selectedRepo && repoData !== 400 && (
         <>
+          Filter by language
+          <FilterContainer>
+            {usedLanguages.map((e) => {
+              return (
+                <FilterButtons
+                  active={filter === e}
+                  onClick={() => {
+                    filterBy(e);
+                  }}
+                  key={e}
+                >
+                  {e}
+                </FilterButtons>
+              );
+            })}
+          </FilterContainer>
           Select a repo
           <Container>
-            {repoData.map((e) => (
+            {renderedRepos.map((e) => (
               <SingleRepo
                 onClick={() => {
                   setSelectedRepo(e);
@@ -79,6 +125,7 @@ const SplashScreen = () => {
   );
 };
 
+//TODO: Styling the components to conform to company standards.
 const Wrapper = styled.div`
   width: 100%;
   height: 100%;
@@ -115,5 +162,23 @@ const SingleRepo = styled.li`
   padding: 0 2rem;
   margin: 0.5rem;
   border: solid lightgray;
+  cursor: pointer;
+`;
+
+const FilterContainer = styled.ul`
+  width: 90%;
+  height: 6rem;
+  border: 2px solid;
+  margin: 1rem 0;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+`;
+
+const FilterButtons = styled.li`
+  cursor: pointer;
+  border: 2px solid;
+  padding: 0.5rem;
+  background-color: ${(props) => (props.active ? 'lightgreen' : '')};
 `;
 export default SplashScreen;
--- web/src/components/ViewRepo.js
+++ web/src/components/ViewRepo.js
@@ -1,19 +1,80 @@
+import { useEffect, useState } from 'react';
 import styled from 'styled-components';
+import ReactMarkdown from 'react-markdown';
+import remarkGfm from 'https://esm.sh/remark-gfm@3';
 
 const ViewRepo = ({ data, setSelectedRepo }) => {
+  const [readMe, setReadme] = useState(false);
+  useEffect(() => {
+    const getReadMe = async () => {
+      await fetch(
+        `https://raw.githubusercontent.com/${data.full_name}/master/README.md`
+      )
+        .then((res) => res.text())
+        .then((res) => setReadme(res));
+    };
+    getReadMe();
+  }, [data]);
+
   return (
-    <Wrapper
-      onClick={() => {
-        setSelectedRepo(false);
-      }}
-    >
-      {data.name}
+    <Wrapper>
+      <GoBack
+        onClick={() => {
+          setSelectedRepo(false);
+        }}
+      >
+        Go back
+      </GoBack>
+      <Title>{data.full_name}</Title>
+      <Message>{data.description}</Message>
+      <Author>By: {data.owner.login}</Author>
+      <LastCommit>Last updated: {data.updated_at}</LastCommit>
+
+      {readMe && (
+        <>
+          {' '}
+          <MarkDownContainer>
+            <ReactMarkdown children={readMe} remarkPlugins={[remarkGfm]} />
+          </MarkDownContainer>
+        </>
+      )}
     </Wrapper>
   );
 };
 
+//TODO: Styling the components to conform to company standards.
 const Wrapper = styled.div`
   width: 90%;
   height: 800px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+const GoBack = styled.button``;
+const Title = styled.h1`
+  font-size: x-large;
+`;
+
+const Message = styled.h2`
+  font-size: medium;
+  margin: 0.25rem 0 1rem 0;
+`;
+
+const Author = styled.h3`
+  margin: 0.25rem 0 1.25rem 0;
+`;
+
+const LastCommit = styled.h3`
+  text-decoration: underline;
+`;
+
+const MarkDownContainer = styled.div`
+  width: 600px;
+  height: 900px;
+  overflow: auto;
+  border: 2px solid;
+  margin: 1rem;
 `;
 export default ViewRepo;
